0fe02f1c0415914aa1e5b91a04f9555b06db3301
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-11-24 16:22:13 +0700

    add email sender

--- src/app/email_manage/email_sender.js
+++ src/app/email_manage/email_sender.js
@@ -3,15 +3,15 @@ const sendEmail = require('./email_transporter');
 module.exports = (app)=>{
     app.route('/send_email')
     .post((req, res) =>{
-        let email = req.session.UserEmail;
-        let subject = req.body.subject;
-        let text = req.body.text;
+        let email = 'haiminhnguyen2001@gmail.com';
+        let subject = 'test';
+        let text = 'test';
         try{
             sendEmail(email, subject, text);
-            res.send(JSON.stringify("email sended"))
+            res.status(200).send(JSON.stringify("email sent"))
         }
         catch(err){
-            res.status(400).send(err);
+            res.status(503).send(err);
         }
     })
 }
