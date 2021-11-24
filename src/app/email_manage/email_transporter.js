c1be458d9441ecfdd37aa4384d7d7e0e3bff635f
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-11-24 16:22:08 +0700

    add email sender

--- src/app/email_manage/email_transporter.js
+++ src/app/email_manage/email_transporter.js
@@ -2,27 +2,32 @@ const cfg = require('./email_cfg')
 const nodemailer = require("nodemailer");
 const user = cfg.user;
 const password = cfg.password;
+const smtpPool = require('nodemailer-smtp-pool');
 
 const sendEmail = async (email, subject, text) => {
     try {
-        let smtpTransport = nodemailer.createTransport("SMTP",{
-            service: "Gmail",
+        let transporter = nodemailer.createTransport(smtpPool({
+            service: 'gmail',
+            host: 'smtp.gmail.com',
             auth: {
                 user: user,
-                pass: password
-            }
-        });
+                password: password,
+            },
+            maxConnections: 5,
+            maxMessages: 100,
+        }))
         let mailOptions = {
             from: user,
             to: email, 
             subject: subject,
             text: text
         }
-        smtpTransport.sendMail(mailOptions, function(error, res){
+        transporter.sendMail(mailOptions, function(error, res){
             if(error){
-                throw err;
+                throw error;
             }
         });
+        console.log('email sent')
     }
     catch(err){
         throw err;
