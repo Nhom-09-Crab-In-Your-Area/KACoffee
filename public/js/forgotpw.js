805c23e8731697d166cb6ec2ad7da9ff836c4298
doducanh2707 <doducanh2707@gmail.com>
2021-12-05 21:10:55 +0700

    .

--- public/js/forgotpw.js
+++ public/js/forgotpw.js
@@ -4,15 +4,19 @@ const codebtn = document.querySelector('#code-submit')
 const submit = document.querySelector('#submit-success')
 submitbtn.addEventListener('click', (e) => {
     e.preventDefault()
+    if()
+    else{
     square.style.transform = 'rotate3d(0, -1, 0, 90deg)'
     document.title = 'Sign up page'
+    }
 })
 codebtn.addEventListener('click', (e) => {
     e.preventDefault()
+
     square.style.transform = 'rotate3d(0, 1, 0, 90deg)'
     document.title = 'Forgot password page'
 })
 submit.addEventListener('click', (e) => {
     e.preventDefault()
-    window.location = '/'
+    window.location = '/login'
 })
