d2b54a4db2a6013fe524f6212270f833ff1c0081
doducanh2707 <doducanh2707@gmail.com>
2021-12-06 22:41:44 +0700

    .

--- public/js/forgotpw.js
+++ public/js/forgotpw.js
@@ -1,11 +1,9 @@
 const square = document.getElementById('square-1')
-const submitbtn = document.querySelector('#email-submit')
-const codebtn = document.querySelector('#code-submit')
+const sendmail = document.querySelector('#email-submit')
 const submit = document.querySelector('#submit-success')
 const email = document.querySelector('#email')
 
-submitbtn.addEventListener('click', checkmail)
-codebtn.addEventListener('click', checkcode)
+sendmail.addEventListener('click', checkmail)
 submit.addEventListener('click', changepassword)
 
 async function checkmail(e) {
@@ -26,21 +24,16 @@ async function checkmail(e) {
                 if ((data = 'email not exist')) {
                     warning.classList.add('d-none')
                     warning1.classList.remove('d-none')
+                } else {
+                    square.style.transform = 'rotate3d(0, -1, 0, 90deg)'
+                    document.title = 'Sign up page'
                 }
             })
-        // square.style.transform = 'rotate3d(0, -1, 0, 90deg)'
-        // document.title = 'Sign up page'
     } else {
         if (!warning1.classList.contains('d-none'))
             warning1.classList.add('d-none')
         warning.classList.remove('d-none')
     }
-}
-async function checkcode(e) {
-    e.preventDefault()
-
-    square.style.transform = 'rotate3d(0, 1, 0, 90deg)'
-    document.title = 'Forgot password page'
 }
 
 async function changepassword(e) {
