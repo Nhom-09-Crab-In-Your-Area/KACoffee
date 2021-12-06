fe280bab518ec60b3846ebe374148da512d61279
doducanh2707 <doducanh2707@gmail.com>
2021-12-06 15:12:32 +0700

    .

--- public/js/forgotpw.js
+++ public/js/forgotpw.js
@@ -3,23 +3,47 @@ const submitbtn = document.querySelector('#email-submit')
 const codebtn = document.querySelector('#code-submit')
 const submit = document.querySelector('#submit-success')
 const email = document.querySelector('#email')
-submitbtn.addEventListener('click', (e) => {
+
+submitbtn.addEventListener('click', checkmail)
+codebtn.addEventListener('click', checkcode)
+submit.addEventListener('click', changepassword)
+
+async function checkmail(e) {
     e.preventDefault()
 
     if (email.value != '') {
-        square.style.transform = 'rotate3d(0, -1, 0, 90deg)'
-        document.title = 'Sign up page'
+        await fetch('/send_email', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email.value,
+            }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                if ((data = 'email not exist')) {
+                    warning.classList.add('d-none')
+                    warning1.classList.remove('d-none')
+                }
+            })
+        // square.style.transform = 'rotate3d(0, -1, 0, 90deg)'
+        // document.title = 'Sign up page'
     } else {
+        if (!warning1.classList.contains('d-none'))
+            warning1.classList.add('d-none')
         warning.classList.remove('d-none')
     }
-})
-codebtn.addEventListener('click', (e) => {
+}
+async function checkcode(e) {
     e.preventDefault()
 
     square.style.transform = 'rotate3d(0, 1, 0, 90deg)'
     document.title = 'Forgot password page'
-})
-submit.addEventListener('click', (e) => {
+}
+
+async function changepassword(e) {
     e.preventDefault()
     window.location = '/login'
-})
+}
