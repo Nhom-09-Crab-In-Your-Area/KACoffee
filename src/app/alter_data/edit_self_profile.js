35a553d7d85f697df2387d218b746851d9c8db15
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-12-13 15:07:18 +0700

    fix bugs

--- src/app/alter_data/edit_self_profile.js
+++ src/app/alter_data/edit_self_profile.js
@@ -5,53 +5,40 @@ const authenticationModel = require("../../models/authentication_model");
 module.exports = (app) => {
   app.route("/edit_self_profile").post(async (req, res) => {
     let update = req.body;
-    if (update.email != req.session.UserEmail){
-     
-      await Model.findOne({'email': update.email}, (err, account) =>{
-        if (err) throw err;
-        else{
-          if (account != null) {
-            res.send(JSON.stringify('email already exist'));
-          }
-        }
-      })
+    console.log('hello')
+    console.log(update)
+
+  
+    let new_password = null;
+    if (update.password) {
+      new_password = update.password;
+      update = Object.assign({}, update, { password: undefined });
+    }
+    if (req.session.AccountType == "Customer") Model = userModel;
+    else {
+      Model = employeeModel;
     }
 
-    else{
-      let new_password = null;
-      if (update.password) {
-        new_password = update.password;
-        update = Object.assign({}, update, { password: undefined });
-        console.log(update);
+    const data = await Model.findOneAndUpdate(
+      { email: req.session.UserEmail },
+      update,
+      {
+        new: true,
       }
-      if (req.session.AccountType == "Customer") Model = userModel;
-      else {
-        Model = employeeModel;
-      }
+    );
 
-      const data = await Model.findOneAndUpdate(
+    if (new_password) {
+      authenticationModel.findOne(
         { email: req.session.UserEmail },
-        update,
-        {
-          new: true,
+        (err, account) => {
+          if (err) throw err;
+          else {
+            account.password = new_password;
+            account.save();
+          }
         }
       );
-
-      //console.log(data);
-      if (new_password) {
-        authenticationModel.findOne(
-          { email: req.session.UserEmail },
-          (err, account) => {
-            if (err) throw err;
-            else {
-              account.password = new_password;
-              account.save();
-            }
-          }
-        );
-      }
-      res.status(200).send(JSON.stringify("OK"));
     }
-    
+    res.status(200).send(JSON.stringify("OK"));    
   });
 };
