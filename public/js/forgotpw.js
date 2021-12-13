35f044167a0dc3e28156c18099bc42263e27a46b
doducanh2707 <doducanh2707@gmail.com>
2021-12-13 15:19:28 +0700

    fix

--- public/js/forgotpw.js
+++ public/js/forgotpw.js
@@ -1,49 +1,8 @@
 const square = document.getElementById('square-1')
-const sendmail = document.querySelector('#email-submit')
-const submitbtn = document.querySelector('#submit')
+const sendmailbtn = document.querySelector('#email-submit')
 const email = document.querySelector('#email')
 
-const password1 = document.querySelector('#password')
-const password2 = document.querySelector('#confirm_password')
-const mesPassword = document.querySelector('#mes-password')
-const mesPassword1 = document.querySelector('#mes-password1')
-const mesPassword2 = document.querySelector('#mes-password2')
-
-submitbtn.addEventListener('click', async (e) => {
-    e.preventDefault()
-    if (password1.value == '') {
-        mesPassword1.classList.remove('mes-none')
-        mesPassword1.classList.add('mes-display')
-    } else if (password2.value == '') {
-        mesPassword1.classList.add('mes-none')
-        mesPassword1.classList.remove('mes-display')
-        mesPassword2.classList.remove('mes-none')
-        mesPassword2.classList.add('mes-display')
-    } else if (password1.value !== password2.value) {
-        mesPassword1.classList.add('mes-none')
-        mesPassword1.classList.remove('mes-display')
-        mesPassword2.classList.add('mes-none')
-        mesPassword2.classList.remove('mes-display')
-        mesPassword.classList.remove('mes-none')
-        mesPassword.classList.add('mes-display')
-    } else {
-        const data = {
-            password: password1.value,
-        }
-        const res = await fetch('/edit_self_profile', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        })
-
-        location.reload()
-    }
-})
-
-sendmail.addEventListener('click', checkmail)
-async function checkmail(e) {
+sendmailbtn.addEventListener('click', async (e) => {
     e.preventDefault()
 
     if (email.value != '') {
@@ -72,4 +31,4 @@ async function checkmail(e) {
             warning1.classList.add('d-none')
         warning.classList.remove('d-none')
     }
-}
+})
