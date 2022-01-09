927206ac01c979005512a892fecd05b63e03e962
MasMas <duongmarcus71@gmail.com>
2022-01-09 13:47:27 +0700

    enable upload file(product)

--- src/models/users_model.js
+++ src/models/users_model.js
@@ -13,6 +13,7 @@ let userSchema = new Schema({
     rank: {type: Number, enum: [0,1,2,3], default: 0},
     totalMoney: {type: Number, default: 0}, // the total money paid
     NbItem: {type: Number, default: 0}, // the number of item which bought
+    NumExchangeItem: {type: Number, default: 0}, // the number of item which is used for getting 5t1 voucher 
     vouchers: [{type: Schema.Types.ObjectId, ref: "Voucher"}]
 }, {collection: 'Users'});
 
