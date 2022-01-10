90939e9b7095e5702f77ae217a703d8035f22913
Phan Anh <phananhtqvn@gmail.com>
2022-01-10 22:47:46 +0700

    fix bug

--- public/js/counter/buy5get1.js
+++ public/js/counter/buy5get1.js
@@ -1,11 +1,11 @@
 document.querySelector('.phoneForm').addEventListener('submit', async (e) => {
     e.preventDefault()
-    console.log(document.getElementsByName('phoneNumber').value)
+    console.log(document.querySelector('#phoneNUMBER').value)
     const res = await fetch('/voucher/get_5t1_voucher', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
-            phone: document.getElementsByName('phoneNumber').value,
+            phone: document.querySelector('#phoneNUMBER').value,
         }),
     }).then((data) => data.json())
     alert(res)
